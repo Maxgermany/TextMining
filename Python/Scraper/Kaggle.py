d88091d0890004ce5399c2cdc73b0520fd4c4b30
Max <maximus.ipod@googlemail.com>
2021-01-04 20:22:18 +0100

    Added functions to divide the big json file with the games and matched the occuring players

---
+++ Python/Scraper/Kaggle.py
@@ -0,0 +1,104 @@
+import ijson  # Module for reading big json-files
+import os
+import json
+import re
+
+
+def splitKaggleData():
+    sourceFile = open("..\\..\\Data\\Kaggle\\games.json", 'r')
+
+    objects = ijson.items(sourceFile, 'item')
+
+    # Filter games that are earlier then 2007
+    games = (game for game in objects if int(game['year']) >= 2007)
+
+    # The result variables (dictioniary)
+    gamesAfterYears = {}
+    playerIDsAfterYears = {}
+
+    # Create neccesary dictionary entry for all years
+    for year in range(2007, 2018):
+        stringYear = str(year)
+        gamesAfterYears[stringYear] = '['  # For json notation
+        playerIDsAfterYears[stringYear] = [] # Player ids are saved in a list
+
+    # Iterate over all games and add them in right section of the dictionary
+    for game in games:
+        gamesAfterYears[game['year']] += str(game) + ","
+        playerIDsAfterYears[game['year']].append(game['player_id'])
+
+    for year in range(2007, 2018):
+        stringYear = str(year)
+        
+        playerIDsAfterYears[stringYear] = list(dict.fromkeys(playerIDsAfterYears[stringYear])) # Remove duplicates
+
+        data = gamesAfterYears[stringYear]
+        data = data[:-1]  # Remove last ","
+        data += "]"  # For json notation
+
+        gamesAfterYears[stringYear] = data
+
+    for year in gamesAfterYears:
+
+        path = "..\\..\\Data\\Kaggle\\"
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        data = gamesAfterYears[year]
+
+        if len(data) < 15: # If there are incorrect value, they are filtered here
+            continue
+
+        # Adjustments for the json
+        data = re.sub("'", '"', data)
+        data = re.sub("True", "true", data)
+        data = re.sub("False", "false", data)
+        data = re.sub('[D][e][c][i][m][a][l][(]["]([0-9]*[.][0-9]*)["][)]', '\\1', data) # Replace 'Decimal("0.0")' with '0.0'
+
+        data = json.loads(data)
+
+        jsonOutputFile = open(path + year + "Games.json", "w")
+        jsonOutputFile.write(json.dumps(data, indent=4))
+        jsonOutputFile.close()
+
+    sourceFile.close()
+
+    #Find the players to the ids
+    findPlayernameToId(playerIDsAfterYears)
+
+def findPlayernameToId(ids):
+
+    # ids has the structure { "year1": [playerid1, playerid2, playerid3...], "year2": [playerid1, playerid2, playerid3...]...}
+
+    sourceFile = open("..\\..\\Data\\Kaggle\\profiles.json", 'r')
+
+    objects = ijson.items(sourceFile, 'item')
+
+    path = "..\\..\\Data\\Kaggle\\"
+
+    for year in ids:
+        data = "{"
+        for playerId in ids[year]:
+            data += '"' + str(playerId) + '":'
+            players = (player for player in objects if int(player["player_id"]) == playerId) # Find the player with the matching id
+            for player in players: # Because ijson gives a list of objects, we need to iterate; even if there is only one element
+                playerFinal = player
+            data += str(playerFinal) + ","
+
+        data = data[:-1]
+        data += "}"
+
+        # json adjustments
+        data = re.sub("'", '"', data)
+        data = re.sub("True", "true", data)
+        data = re.sub("False", "false", data)
+        data = re.sub("None", '"None"', data)
+
+        data = json.loads(data)
+
+        jsonOutputFile = open(path + year + "Players.json", "w")
+        jsonOutputFile.write(json.dumps(data, indent=4))
+        jsonOutputFile.close()
+
+splitKaggleData()
