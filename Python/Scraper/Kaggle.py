573262621fd69160c73a58999513f1340d25b770
Max <maximus.ipod@googlemail.com>
2021-01-14 21:23:39 +0100

    Added function to split the data further in weeks of a season and generated the data

--- Python/Scraper/Kaggle.py
+++ Python/Scraper/Kaggle.py
@@ -2,14 +2,16 @@ import ijson  # Module for reading big json-files
 import os
 import json
 import re
+import datetime
 
 
 def splitKaggleData(splitYear):
-    sourceFile = open("..\\..\\Data\\Kaggle\\games.json", 'r') # Remember to unzip the file and remove it after processing, because it is to big for Github
+    sourceFile = open("..\\..\\Data\\Kaggle\\games.json",
+                      'r')  # Remember to unzip the file and remove it after processing, because it is to big for Github
 
     objects = ijson.items(sourceFile, 'item')
 
-    # Filter games that are earlier then 2007
+    # Filter games that are in the given year
     games = (game for game in objects if int(game['year']) == splitYear)
 
     # The result variables (dictioniary)
@@ -20,7 +22,7 @@ def splitKaggleData(splitYear):
     for year in range(splitYear, splitYear + 1):
         stringYear = str(year)
         gamesAfterYears[stringYear] = '['  # For json notation
-        playerIDsAfterYears[stringYear] = [] # Player ids are saved in a list
+        playerIDsAfterYears[stringYear] = []  # Player ids are saved in a list
 
     # Iterate over all games and add them in right section of the dictionary
     for game in games:
@@ -29,8 +31,8 @@ def splitKaggleData(splitYear):
 
     for year in range(splitYear, splitYear + 1):
         stringYear = str(year)
-        
-        playerIDsAfterYears[stringYear] = list(dict.fromkeys(playerIDsAfterYears[stringYear])) # Remove duplicates
+
+        playerIDsAfterYears[stringYear] = list(dict.fromkeys(playerIDsAfterYears[stringYear]))  # Remove duplicates
 
         data = gamesAfterYears[stringYear]
         data = data[:-1]  # Remove last ","
@@ -47,14 +49,15 @@ def splitKaggleData(splitYear):
 
         data = gamesAfterYears[year]
 
-        if len(data) < 15: # If there are incorrect value, they are filtered here
+        if len(data) < 15:  # If there are incorrect value, they are filtered here
             continue
 
         # Adjustments for the json
         data = re.sub("'", '"', data)
         data = re.sub("True", "true", data)
         data = re.sub("False", "false", data)
-        data = re.sub('[D][e][c][i][m][a][l][(]["]([0-9]*[.][0-9]*)["][)]', '\\1', data) # Replace 'Decimal("0.0")' with '0.0'
+        data = re.sub('[D][e][c][i][m][a][l][(]["]([0-9]*[.][0-9]*)["][)]', '\\1',
+                      data)  # Replace 'Decimal("0.0")' with '0.0'
 
         data = json.loads(data)
 
@@ -64,11 +67,14 @@ def splitKaggleData(splitYear):
 
     sourceFile.close()
 
-    #Find the players to the ids
+    # Find the players to the ids
     findPlayernameToId(playerIDsAfterYears)
 
-def findPlayernameToId(ids):
+    #Split the data further in gameWeeks
+    splitYearAfterWeek(splitYear)
 
+
+def findPlayernameToId(ids):
     # ids has the structure { "year1": [playerid1, playerid2, playerid3...], "year2": [playerid1, playerid2, playerid3, ...], ...}
 
     path = "..\\..\\Data\\Kaggle\\"
@@ -98,6 +104,130 @@ def findPlayernameToId(ids):
         jsonOutputFile.write(json.dumps(data, indent=4))
         jsonOutputFile.close()
 
+#Splits the year data in to
+def splitYearAfterWeek(year):
+
+    calendarWeeks = getCalenderWeeksAsArray(year)
+
+    if year != 2017:  # For year 2017 is the datase only
+
+        nflWeekNumber = 18  # A season goes from year to the other, so year x has the last 4 weeks of year x-1
+        pathYear = year - 1  # Same reason as above
+
+        for week in calendarWeeks:
+
+            if nflWeekNumber == 22:  # At this point the games are of the new season of the given year
+                pathYear = year
+                nflWeekNumber = 1
+
+            weekGames = '['
+
+            sourceFile = open("..\\..\\Data\\Kaggle\\" + str(year) + "Games.json", 'r')
+
+            games = ijson.items(sourceFile, 'item')
+
+            # Filter games that are in the given week of the year
+            gameFilter = (game for game in games if getCalendarWeek(game['date']) == week)
+
+            for game in gameFilter:
+                weekGames += str(game) + ","
+
+            weekGames = weekGames[:-1]
+            weekGames += "]"
+
+            sourceFile.close()
+
+            data = weekGames
+
+            # Adjustments for the json
+            data = re.sub("'", '"', data)
+            data = re.sub("True", "true", data)
+            data = re.sub("False", "false", data)
+            data = re.sub('[D][e][c][i][m][a][l][(]["]([0-9]*[.][0-9]*)["][)]', '\\1',
+                          data)  # Replace 'Decimal("0.0")' with '0.0'
+
+            data = json.loads(data)
+
+            path = "..\\..\\Data\\Kaggle\\" + str(pathYear) + "\\"
+
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+            jsonOutputFile = open(path + "week" + str(nflWeekNumber) + ".json", "w")
+            jsonOutputFile.write(json.dumps(data, indent=4))
+            jsonOutputFile.close()
+
+            nflWeekNumber += 1
+
+    else:
+        nflWeekNumber = 1
+        for week in calendarWeeks:
+
+            weekGames = '['
+
+            sourceFile = open("..\\..\\Data\\Kaggle\\" + str(year) + "Games.json", 'r')
+
+            games = ijson.items(sourceFile, 'item')
+
+            # Filter games that are in the given year
+            gameFilter = (game for game in games if getCalendarWeek(game['date']) == week)
+
+            for game in gameFilter:
+                weekGames += str(game) + ","
+
+            weekGames = weekGames[:-1]
+            weekGames += "]"
+
+            sourceFile.close()
+
+            data = weekGames
+
+            # Adjustments for the json
+            data = re.sub("'", '"', data)
+            data = re.sub("True", "true", data)
+            data = re.sub("False", "false", data)
+            data = re.sub('[D][e][c][i][m][a][l][(]["]([0-9]*[.][0-9]*)["][)]', '\\1',
+                          data)  # Replace 'Decimal("0.0")' with '0.0'
+
+            data = json.loads(data)
+
+            path = "..\\..\\Data\\Kaggle\\" + str(year) + "\\"
+
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+            jsonOutputFile = open(path + "week" + str(nflWeekNumber) + ".json", "w")
+            jsonOutputFile.write(json.dumps(data, indent=4))
+            jsonOutputFile.close()
+
+            nflWeekNumber += 1
+
+
+def getCalendarWeek(date):
+    dateParts = date.split("-")
+    calendarWeek = datetime.date(int(dateParts[0]), int(dateParts[1]), int(dateParts[2])).isocalendar()[1]
+    return calendarWeek
+
+
+def getCalenderWeeksAsArray(year):
+    sourceFile = open("..\\..\\Data\\Kaggle\\" + str(year) + "Games.json", 'r')
+
+    games = ijson.items(sourceFile, 'item')
+
+    calendarWeeks = []
+
+    for game in games:
+        gameDate = game['date']
+        calendarWeek = getCalendarWeek(gameDate)
+        calendarWeeks.append(calendarWeek)
+
+    sourceFile.close()
+
+    calendarWeeks = list(set(calendarWeeks))  # Remove duplicates
+
+    return calendarWeeks
+
+
 for year in range(2008, 2018):
     splitKaggleData(year)
-    print("Splitted year " + year + ".")
+    print("Scrapped year " + str(year) + ".")
