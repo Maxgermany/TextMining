76541fc06a411f84b6b072e9d5acc94990b77e86
Max <maximus.ipod@googlemail.com>
2021-01-17 17:40:33 +0100

    Added comments to code

--- R/MapNumbers.R
+++ R/MapNumbers.R
@@ -1,6 +1,7 @@
 library(stringr)
 library(rjson)
 
+# Function extracts all numbers from given text
 numExtract <- function(string) {
   allNumbers <- regmatches(string, gregexpr("[[:digit:]]+", string))
   allNumbers <- as.numeric(unlist(allNumbers))
@@ -12,7 +13,7 @@ weeks <- list("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", 
 
 years <- list("2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017")
 
-#Creating the Folders if not exists
+# Creating the Folders if not exists
 path <- "..\\Data\\Output"
 if (!(dir.exists(path))){
   dir.create(path)
@@ -26,32 +27,34 @@ for (year in years) {
   }
 }
 
+
 for (year in years) {
   
   for (week in weeks) {
     
-    weekFile <- paste(c("..\\Data\\RMoreInformation\\", year, "\\week_", week, ".json"), collapse = "") # File with the information of the week and the comments
+    weekFile <- paste(c("..\\Data\\RMoreInformation\\", year, "\\week_", week, ".json"), collapse = "") # File with the information of the game
     
     if (file.exists(weekFile)) {
       
       resultWeek <- rjson::fromJSON(file = weekFile)
       
-      i <- 1
+      i <- 1 # For iteration purpsoe
       
       for (game in resultWeek$games) {
         
-        numbers <- numExtract(game$comments)
+        numbers <- numExtract(game$comments) # Get numbers from text
         
         j <- 1
         
-        if (length(game$players) > 1){
+        if (length(game$players) > 1){ # Games which doesn't have any players are filtered here
           
           for (player in game$players) {
             
-            playerInformation <- player$`game information`
+            playerInformation <- player$`game information` # Get the detailed information of the player for this game
             
             properties <- paste(names(playerInformation), playerInformation)
             
+            # Check for each number if it occurs in the statistics of the player
             for (number in numbers) {
               
               for (property in properties) {
@@ -64,7 +67,7 @@ for (year in years) {
                   if (number != 0) {
                     
                     playerInformation[[propertyName]] <- list(propertyValue, 13)
-                    names(playerInformation[[propertyName]]) <- c("propertyValue", "annotationID")
+                    names(playerInformation[[propertyName]]) <- c("propertyValue", "annotationID") # Add marker for annotation, if match is found
                   
                   }
                 
@@ -86,11 +89,11 @@ for (year in years) {
           
       }
       
-      exportJSON <- rjson::toJSON(resultWeek, 1) #Generate JSON
+      exportJSON <- rjson::toJSON(resultWeek, 1) # Generate JSON
       
       fileName <- paste(c("..\\Data\\Output\\", year, "\\week_", week, ".json"), collapse = "")
       
-      write(exportJSON, fileName) #Save JSON to file
+      write(exportJSON, fileName) # Save JSON to file
       
     }
     
